22966b22f7263fbb06c0218a16178ff25a573275
Joseph Spadavecchia <joseph@redtrie.com>
2021-11-18 22:48:40 +0000

    Implemented simple example constraint system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use kimchi_circuits::{
+    gate::{CircuitGate, GateType},
+    nolookup::constraints::ConstraintSystem,
+    wires::Wire,
+};
+
+use mina_curves::pasta::fp::Fp;
+
 fn main() {
-    let cs = ConstraintSystem::<Fp>::create(gates, vec![], fp_sponge_params, public).unwrap();
+    let gates = vec![
+        CircuitGate {
+            row: 0,
+            typ: GateType::Generic, // ql*L + qr*R + qo*O + qm*L*R + qc*C = 0
+            wires: Wire::new(0),
+            c: vec![
+                // selector coefficients for V - E + F = 2
+                Fp::from(1),  // ql
+                Fp::from(-1), // qr
+                Fp::from(-2), // qo
+                Fp::from(0),  // qm
+                Fp::from(1),  // qc
+            ],
+        },
+        CircuitGate {
+            row: 1,
+            typ: GateType::Generic, // ql*L + qr*R + qo*O + qm*L*R + qc*C = 0
+            wires: Wire::new(1),
+            c: vec![
+                // selector coefficients for Y = 3X + B
+                Fp::from(3),  // ql
+                Fp::from(1),  // qr
+                Fp::from(-1), // qo
+                Fp::from(0),  // qm
+                Fp::from(0),  // qc
+            ],
+        },
+    ];
+
+    let cs = ConstraintSystem::create(gates, vec![], oracle::pasta::fp::params(), 0).unwrap();
+
+    println!("cs = {:?}", cs);
 }
